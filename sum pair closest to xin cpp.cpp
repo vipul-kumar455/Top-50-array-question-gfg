9e52b12689083af7464e4de07b2f825d5235e0f6
Vipul Kumar <74072895+Cyber321-glit-majer@users.noreply.github.com>
2022-07-15 22:14:11 +0530

    Add files via upload

---
+++ sum pair closest to xin cpp.cpp
@@ -0,0 +1,33 @@
+lass Solution{   
+public:
+    vector<int> sumClosest(vector<int>arr, int x)
+    {
+       int n=arr.size();
+       int mi=INT_MAX;
+       int sum=0;
+      
+       int i=0;
+       int j=n-1;
+        int f=i,s=j;
+        while(i<j)
+        {
+            sum=arr[i]+arr[j];
+            if(abs(arr[i]+arr[j]-x)<mi)
+            {
+               mi=abs(arr[i]+arr[j]-x);
+               f=i;
+               s=j;
+            }
+            if(sum>x)
+            {
+                j--;
+            }
+            else
+            {
+                i++;
+            }
+            
+        }
+        return {arr[f],arr[s]};
+    }
+};
